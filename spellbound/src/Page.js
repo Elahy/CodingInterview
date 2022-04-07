2cdaa2223fde39f6c8633a88b1630d29cda36415
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2022-04-07 14:38:05 +0600

    all pages added

--- spellbound/src/Page.js
+++ spellbound/src/Page.js
@@ -15,6 +15,16 @@ import Videos from "./components/Videos";
 import Ports from "./components/Ports";
 import Tunnels from "./components/Tunnels";
 import Powering from "./components/Powering";
+import TechSector from "./components/TechSector";
+import YoungPopulations from "./components/YoungPopulations";
+import Ecommerce from "./components/Ecommerce";
+import Startups from "./components/Startups";
+import Fintech from "./components/Fintech";
+import GreenBonds from "./components/GreenBonds";
+import Sustainable from "./components/Sustainable";
+import ImprovingRegulations from "./components/ImprovingRegulations";
+import SolarPower from "./components/SolarPower";
+import StockMarkets from "./components/StockMarkets";
 
 function Page() {
   return (
@@ -32,7 +42,17 @@ function Page() {
       <Ports />
       <Powering />
       <Goals />
+      <TechSector />
+      <YoungPopulations />
+      <Ecommerce />
+      <Startups />
+      <Fintech />
       <Emerging />
+      <GreenBonds />
+      <Sustainable />
+      <ImprovingRegulations />
+      <SolarPower />
+      <StockMarkets />
       <Ending />
       <Footer />
     </>
